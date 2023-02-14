a7c39af25ce3bfc35a86c852e30778446585e7d0
Ivan Bowen <bowenivan16@gmail.com>
2023-02-15 00:43:41 +0300

    initial project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {  useColorMode, Button } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <header className="App-header">
+        <Button onClick={toggleColorMode}>
+          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
+        </header>
+        <p>This is Ivan Typing</p>
+      </div>
   );
 }
 

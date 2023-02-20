c71eb18b8adbcd445ca59f60a280e6cd06d769d0
Ivan Bowen <bowenivan16@gmail.com>
2023-02-20 17:48:42 +0300

    import profile component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useColorMode } from "@chakra-ui/react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
 
 function App() {
 	const { colorMode, toggleColorMode } = useColorMode();
-
 	return (
 		<div className="App">
 			<Navbar colorMode={colorMode} toggleColorMode={toggleColorMode} />
+			<Landing />
 		</div>
 	);
 }
